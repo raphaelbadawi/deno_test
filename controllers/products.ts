7608f02169cc6687746a3dba6750b3124f04ed2d
Raphael Badawi <contact@raphaelbadawi.fr>
2021-04-19 15:15:05 +0200

    strange bug happening with addProduct, investigating...

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,26 +1,27 @@
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 import { Product } from "../types.ts";
 
 let products: Product[] = [
   {
-    id: 1,
+    id: "1",
     name: "Cat One",
     description: "This is the first cat on the cat",
     price: 0.99,
   },
   {
-    id: 2,
+    id: "2",
     name: "Cat Two",
     description: "This is the second cat on the cat",
     price: 1.99,
   },
   {
-    id: 1,
+    id: "3",
     name: "Cat Three",
     description: "This is the third cat on the cat",
     price: 2.99,
   },
   {
-    id: 1,
+    id: "4",
     name: "Cat Four",
     description: "This is the fourth cat on the cat",
     price: 3.99,
@@ -42,16 +43,58 @@ const getProducts = ({ response }: { response: any }) => {
  * @desc Get single product
  * @route GET /api/v1/products/:id
  */
-const getProduct = ({ response }: { response: any }) => {
-  response.body = "get";
+const getProduct = ({
+  params,
+  response,
+}: {
+  params: { id: string };
+  response: any;
+}) => {
+  const product: Product | undefined = products.find((p) => p.id === params.id);
+  if (product) {
+    response.status = 200;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  } else {
+    response.status = 404;
+    response.body = {
+      success: false,
+      msg: "No matching product found",
+    };
+  }
 };
 
 /*
  * @desc Add product
  * @route POST /api/v1/products
  */
-const addProduct = ({ response }: { response: any }) => {
-  response.body = "add";
+// async keyword because the await keyword is not in the global scope
+const addProduct = async ({
+  request,
+  response,
+}: {
+  request: any;
+  response: any;
+}) => {
+  const body = await request.body();
+  if (!request.hasBody) {
+    response.status = 400;
+    response.body = {
+      success: false,
+      msg: "No data",
+    };
+  } else {
+    const product: Product = body.value;
+    product.id = v4.generate();
+    products.push(product);
+    response.status = 201;
+    response.body = {
+      success: true,
+      data: product,
+    };
+  }
 };
 
 /*
